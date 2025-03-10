962719dfa3a7b594fcb2c41002300212419fe271
Nikhil-ism <nikhilchandel20032003@gmail.com>
2025-03-10 15:49:21 +0530

    book collection full project

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.sql.SQLOutput;
 import java.util.*;
+import java.io.*;
+
 public class Main {
 
+    public static final String FILE_NAME = "library.txt";
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
         //ask inout for no. of books
@@ -11,7 +14,7 @@ public class Main {
         System.out.println("Enter the no. of books: ");
         int numberOfBooks = sc.nextInt();
         sc.nextLine();
-        Book[] book = new Book[numberOfBooks];
+        Book[] books = new Book[numberOfBooks];
 
         for(int i=0; i<numberOfBooks; i++){
             System.out.println("\nEnter details for Book " + (i + 1) + ":");
@@ -22,16 +25,52 @@ public class Main {
             System.out.print("Enter ISBN: ");
             String isbn = sc.nextLine();
 
-            book[i] = new Book(title,author,isbn);
+            books[i] = new Book(title,author,isbn);
 
         }
+        saveBooksToFile(books);
+        readBooksFromFile();
         System.out.println("\nLibrary Books:");
-        for (int i = 0; i < book.length; i++) {
+        for (int i = 0; i < books.length; i++) {
             System.out.println("\nBook " + (i + 1) + ":");
-            book[i].displayBookInfo();
+            books[i].displayBookInfo();
         }
 
 
         sc.close();
+    }
+
+    public static void saveBooksToFile(Book[] books){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))){
+            for(Book book : books){
+                writer.write(book.getTitle() + "," + book.getAuthor() + "," + book.getIsbn());
+                writer.newLine();
+            }
+            System.out.println("Books saved to " + FILE_NAME);
+        }catch(IOException e){
+            System.out.println("Error occurred while saving the file");
+        }
+    }
+
+
+
+    public static void readBooksFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            System.out.println("books stored in file");
+            while ((line = reader.readLine()) != null) {
+                String[] bookDetails = line.split(",");
+                if (bookDetails.length == 3) {
+                    System.out.println("Title: " + bookDetails[0]);
+                    System.out.println("Author: " + bookDetails[1]);
+                    System.out.println("ISBN: " + bookDetails[2]);
+                    System.out.println("----------------------");
+                } else {
+                    System.out.println("Invalid data format: " + line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the books.");
+        }
     }
 }
